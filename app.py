f11b8ab8b7582567a74f15ef238aa92676338863
konerunatwar <konerunatwar@gmail.com>
2021-05-15 10:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,21 +19,25 @@ def get_estimated_price(property_size,bhk,property_age,gym,lift,swimmingPool,loc
     x[0] = property_size
     x[1] = bhk
     x[2] = property_age
-    x[3] = gym
+    
     if gym == 'no':
         gym=0
     if gym == 'yes':
         gym=1
-    x[4] = lift
+    x[3] = gym
+    
     if lift == 'no':
         lift=0
     if lift == 'yes':
         lift=1
-    x[5] = swimmingPool
+    x[4] = lift
+    
     if swimmingPool == 'no':
         swimmingPool=0
     if swimmingPool == 'yes':
         swimmingPool=1
+    x[5] = swimmingPool
+    
     x[loc_index] = 1
     less = round(model.predict([x])[0],2)
     if less>= 35000 and bhk >= 3 and property_size >=2500 :
